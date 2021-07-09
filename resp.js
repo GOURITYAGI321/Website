4782f77f8b315ba8cd859e871bb5ffd365c63a21
GOURITYAGI321 <75690560+GOURITYAGI321@users.noreply.github.com>
2021-07-09 12:26:41 +0530

    Add files via upload

---
+++ resp.js
@@ -0,0 +1,11 @@
+burger=document.querySelector('.burger');
+burger=document.querySelector('.navbar');
+burger=document.querySelector('.nav-list');
+burger=document.querySelector('.rightnav');
+
+burger.addEventListner('click',()=>{
+    rightnav.classList.toggle('v-class-resp');
+    nav-list.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+}
+)
